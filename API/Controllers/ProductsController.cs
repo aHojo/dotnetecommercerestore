e579d39b49ca726099a074f65201c085e3b9e098
Andrew Hojnowski <ahojodev@gmail.com>
2022-05-01 19:30:58 -0400

    Made responses async

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -13,16 +14,17 @@ namespace API.Controllers
   public class ProductsController : ControllerBase
   {
     // dependency injections
-    private readonly StoreContext context;
+    private readonly StoreContext _context;
     public ProductsController(StoreContext context)
     {
-      this.context = context;
+      this._context = context;
     }
 
     [HttpGet]
-    public ActionResult<List<Product>> GetProducts()
+    public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = context.Products.ToList();
+      // var products =  context.Products.ToList();
+      var products = await _context.Products.ToListAsync();
 
       return Ok(products);
     }
@@ -30,9 +32,10 @@ namespace API.Controllers
     // get the id parameter
     [HttpGet("{id}")] // api/products/3
                       // in the params id is from the path
-    public ActionResult<Product> GetProduct(int id)
+    public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      return context.Products.Find(id);
+      // return context.Products.Find(id);
+      return await _context.Products.FindAsync(id);
     }
   }
 }
